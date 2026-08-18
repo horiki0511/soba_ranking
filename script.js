1c3b17e51c04c46bc73c58d7c131f6ef82f53301
堀木裕史 <y.horiki@dm-s.co.jp>
2026-08-18 17:53:16 +0900

    0818

---
+++ script.js
@@ -0,0 +1,24 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+    alert("JS読み込みOK");
+
+    const ratings = document.querySelectorAll(".rating");
+
+    setTimeout(() => {
+
+        ratings.forEach((rating) => {
+
+            const scoreElement = rating.querySelector(".data-score");
+            const point = rating.querySelector(".point");
+
+            if (!point) return;
+
+            const score = Number(scoreElement.textContent);
+            const position = (score / 5) * 100;
+
+            point.style.left = position + "%";
+        });
+
+    }, 200);
+
+});
